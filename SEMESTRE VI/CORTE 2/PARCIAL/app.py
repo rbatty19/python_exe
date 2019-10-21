01d0b2354bf338aa70c8559dc49fcf36dca1f910
Rober19 <texas_telecaster@hotmail.com>
2019-10-21 13:10:12 -0500

    [Rober19] [2019/10/21] mejorado

--- SEMESTRE VI/CORTE 2/PARCIAL/app.py
+++ SEMESTRE VI/CORTE 2/PARCIAL/app.py
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/empleados.db'
 
 db = SQLAlchemy(app)
 
+
 class Empleado(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     numero = db.Column(db.String(200))
